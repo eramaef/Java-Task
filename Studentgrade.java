e704a149961d29389e8061299fccc32d1183744c
Era Mae Fulgencio <148873991+eramaef@users.noreply.github.com>
2023-11-15 12:02:06 +0000

    done

---
+++ Studentgrade.java
@@ -0,0 +1,21 @@
+
+
+public class StudentGrade {
+    public static void main(String[] args) {
+        int score =(55 );
+        
+        if (score >= 90 && score <= 100) 
+            System.out.println("A grade ");
+         else if (score >= 80 && score <= 89) 
+            System.out.println("B grade");
+         else if (score >= 70 && score <= 79) 
+            System.out.println("C grade");
+         else if (score >= 60 && score <= 69) 
+            System.out.println("D grade");
+         else 
+            System.out.println("Grade: F");
+        
+        
+        
+    }
+}
