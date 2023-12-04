3fc2089df3d3100eafb1bd71e73a0d96eaf07631
Era Mae Fulgencio <148873991+eramaef@users.noreply.github.com>
2023-12-04 14:01:04 +0000

    done adding output

---
+++ Book.java
@@ -0,0 +1,33 @@
+public class Book {
+    String title;
+    String author;
+    int yearPublished;
+    double price;
+
+    public Book(String title, String author, int yearPublished, double price) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+}
+public class Book  {
+    public static void main(String[] args) {
+        Book book1 = new Book("Java Programming", "John Smith", 2021, 39.99);
+        Book book2 = new Book("Python Basics", "Jane Doe", 2020, 29.99);
+        Book book3 = new Book("C++ Essentials", "Michael Johnson", 2019, 49.99);
+
+        printBookDetails("Book 1", book1);
+        printBookDetails("Book 2", book2);
+        printBookDetails("Book 3", book3);
+    }
+
+    public static void printBookDetails(String bookName, Book book) {
+        System.out.println(bookName + ":");
+        System.out.println("Title: \"" + book.title + "\"");
+        System.out.println("Author: \"" + book.author + "\"");
+        System.out.println("Year Published: " + book.yearPublished);
+        System.out.println("Price: $" + book.price);
+        System.out.println();
+    }
+}
--- Studentgrade.java
+++ Studentgrade.java
@@ -1,6 +1,6 @@
 
 
-public class StudentGrade {
+public class Studentgrade {
     public static void main(String[] args) {
         int score =(55 );
         
